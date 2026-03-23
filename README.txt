a121fd0d730be67a2b4aa92e4b2aec2f47f39e15
minjae0929-ui <minjae0929@gmail.com>
2026-03-23 17:31:15 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+Neocity Survivor - Modes & Levels build
+STANDARD 10분 생존으로 다음 레벨 해금.
+BLITZ/ENDLESS 포함.
---
+++ sw.js
@@ -0,0 +1,6 @@
+/* v5 */
+const CACHE="neocity-survivor-levels-v4-records";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener(-blitz1m-shield100"activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
